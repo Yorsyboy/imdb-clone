195381ec6ac4e132542981f19968067a8f26a26a
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-02-03 19:19:48 +0100

    update card and navbar

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,25 +1,37 @@
 import Image from "next/image";
-import { ThumbUpIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
 
 export default function Card({ result }) {
-  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="p-3 cursor-pointer active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
-      <Image
-        src={BASE_URL + result.backdrop_path}
-        layout="responsive"
-        width={200}
-        height={100}
-        alt="Movie Image"
-      />
-      <div className="p-2">
-        <p className="truncate text-lg">{result.overview}</p>
-        <h2 className="text-lg font-bold">{result.title || result.name}</h2>
-        <p className="flex items-center">
-          {result.release_date || result.first_air_date}
-          <ThumbUpIcon className="h-5  ml-3 mr-1" /> {result.vote_count}
-        </p>
-      </div>
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          placeholder="blur"
+          blurDataURL={`https://image.tmdb.org/t/p/original/${result.poster_path}`}
+          alt="image is not available"
+        ></Image>
+        <div className="p-2">
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <h2 className="truncate text-lg font-bold">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
